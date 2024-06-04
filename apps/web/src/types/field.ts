a944575ba2b9d6b21b249808cba2adf8863eb864
Lucas Paulo <lucaspaulodev@gmail.com>
2024-06-04 18:42:06 -0300

    Fixing type errors.

--- apps/web/src/types/field.ts
+++ apps/web/src/types/field.ts
@@ -3,8 +3,8 @@ import React, { ChangeEvent } from 'react';
 export interface FormFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   variant?: 'value' | 'counter' | 'chips';
   label?: string;
-  value: number | string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  value?: number | string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export interface FieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
