ab6e65911886901f7fbc03fbaeeb2f124d0b0622
Lucas Paulo <lucaspaulodev@gmail.com>
2024-06-06 16:36:44 -0300

    Adding Math.max(Number(value) - 1, 0) to CounterFields in order to prevent negative values being set.

--- apps/web/src/components/Home/CounterField.tsx
+++ apps/web/src/components/Home/CounterField.tsx
@@ -6,12 +6,12 @@ import { ChangeEvent } from "react";
 
 const CounterField = ({ label, value, onChange = () => {}, ...props }: FieldProps) => {
   const handleIncrement = () => {
-    onChange({ target: { value: (Number(value) + 1).toString() } } as ChangeEvent<HTMLInputElement>);
+    onChange({ target: { value: Math.max(Number(value) - 1, 0).toString() } } as ChangeEvent<HTMLInputElement>);
   };
 
   const handleDecrement = () => {
     if (Number(value) > 0) {
-      onChange({ target: { value: (Number(value) - 1).toString() } } as ChangeEvent<HTMLInputElement>);
+      onChange({ target: { value: Math.max(Number(value) - 1, 0).toString() } } as ChangeEvent<HTMLInputElement>);
     }
   };
 
